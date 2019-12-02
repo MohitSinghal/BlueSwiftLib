576d250c870b5ef49fe07bc0c0dd3f69edd97615
mohit kumar <mohit.kumar@spirit.com>
2019-12-02 14:41:49 +0530

    Intial project setup

---
+++ BlueSwiftLib/BlueSwiftLib/BlueSwiftLib.h
@@ -0,0 +1,19 @@
+//
+//  BlueSwiftLib.h
+//  BlueSwiftLib
+//
+//  Created by Mohit Kumar - NTL on 02/12/19.
+//  Copyright © 2019 Mohit Kumar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BlueSwiftLib.
+FOUNDATION_EXPORT double BlueSwiftLibVersionNumber;
+
+//! Project version string for BlueSwiftLib.
+FOUNDATION_EXPORT const unsigned char BlueSwiftLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BlueSwiftLib/PublicHeader.h>
+
+
---
+++ BlueSwiftLib/BlueSwiftLibExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlueSwiftLibExample
+//
+//  Created by Mohit Kumar - NTL on 02/12/19.
+//  Copyright © 2019 Mohit Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
